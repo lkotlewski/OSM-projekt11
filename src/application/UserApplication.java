506593255804fa16b3d8f492f5f47d7a42c3d114
Łukasz Kotlewski <luckyyplayer@gmail.com>
2017-03-22 19:24:20 +0100

    Dodany formularz do wprowadzania danych pacjenta. Usunięty zbędny kod oraz kod zakomentowany.

--- src/application/UserApplication.java
+++ src/application/UserApplication.java
@@ -4,40 +4,142 @@ import javafx.application.Application;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.util.Callback;
+import javafx.scene.layout.*;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TableColumn;
+import javafx.scene.control.*;
 import javafx.scene.control.TableColumn.CellDataFeatures;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 public class UserApplication extends Application {
 
 	/**
 	 * Przyciks button odpowiedzialny za...
 	 */
-	public Button button;
-	public Label label;
-	public TextField text;
 	public Menu menu = new Menu("Aplikacja");
 	public MenuBar menuBar = new MenuBar();
-	public Controller controller = new Controller();
+	public MenuItem menuItem = new MenuItem("Wyjœcie");
 	public TableView<UserData> table  = new TableView<UserData>();
 	public Model model = new Model();
+	public Controller controller = new Controller();
 	
 
+	
+	
+
+	
+	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see javafx.application.Application#start(javafx.stage.Stage)
+	 */
+	public void start(Stage primaryStage)
+	{
+		try 
+		{
+			createTable();
+			BorderPane mainLayout = new BorderPane();
+			mainLayout.setPadding(new Insets(0,20,0,0)); // margines z prawej strony
+			
+			VBox rightLayout = new VBox(); //opakowuje tablice
+			rightLayout.getChildren().add(table);
+			mainLayout.setRight(rightLayout);
+			
+			BorderPane leftLayout = new BorderPane(); // opakowuje dwa obszary po lewej stronie
+			leftLayout.setMinSize(300,400);
+			leftLayout.setPadding(new Insets(20, 20, 20 ,20));
+			mainLayout.setLeft(leftLayout);
+			
+			GridPane formGrid = new GridPane();
+			formGrid.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,
+					CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+			formGrid.setMinSize(260, 240);
+			formGrid.setPadding(new Insets(20, 20, 20 ,20));
+	        formGrid.setAlignment(Pos.TOP_CENTER);
+	        formGrid.setHgap(20);
+	        formGrid.setVgap(10);
+	        //formGrid.setGridLinesVisible(true);
+	        
+	        Text formTitle = new Text("Dane Pacjenta");
+	        formGrid.add(formTitle, 0, 0, 2, 1);
+	    
+
+	        Label name = new Label("Imiê:");
+	        formGrid.add(name, 0, 1, 2, 1);
+
+	        TextField nameTextField = new TextField();
+	        formGrid.add(nameTextField, 2, 1, 2, 1);
+	        
+	        Label surname = new Label("Nazwisko:");
+	        formGrid.add(surname, 0, 2, 2, 1);
+
+	        TextField surnameTextField = new TextField();
+	        formGrid.add(surnameTextField, 2, 2, 2, 1);
+	        
+	        Label pesel = new Label("PESEL:");
+	        formGrid.add(pesel, 0, 3, 2, 1);
+
+	        TextField peselTextField = new TextField();
+	        formGrid.add(peselTextField, 2, 3, 2, 1);
+	        
+	        Label sex = new Label("P³eæ:");
+	        formGrid.add(sex, 0, 4, 2, 1);
+	        
+	       
+	        RadioButton femaleCheck = new RadioButton("Kobieta");
+	        RadioButton maleCheck = new RadioButton("Mê¿czyzna");
+	        formGrid.add(femaleCheck, 2, 4);
+	        formGrid.add(maleCheck, 3, 4);
+	        ToggleGroup sexGroup = new ToggleGroup();
+	        femaleCheck.setToggleGroup(sexGroup);
+	        maleCheck.setToggleGroup(sexGroup);
+	        
+	        Label insurance = new Label("Ubezpieczenie");
+	        formGrid.add(insurance, 0, 5, 2, 1);
+	        
+	        ComboBox<String> insuranceType = new ComboBox<String>();
+	        formGrid.add(insuranceType, 3, 5, 2, 1);
+	        insuranceType.getItems().addAll("NFZ","Prywatne","Brak");
+	        insuranceType.getSelectionModel().selectFirst();
+	        Button saveButton = new Button("Zapisz");
+	        Button clearButton = new Button("Anuluj");
+	        
+	        formGrid.add(saveButton, 0, 6);
+	        formGrid.add(clearButton, 1, 6);
+	        
+			leftLayout.setTop(formGrid);
+			menuItem.setOnAction(e->
+			{
+				
+				primaryStage.close();
+				
+			});
+			
+			menu.getItems().add(menuItem);
+			menuBar.getMenus().add(menu);
+			
+			mainLayout.setTop(menuBar);
+		
+			Scene scene = new Scene(mainLayout, 1200, 500);
+			primaryStage.setTitle("Rejestracja wyników badañ");
+			primaryStage.setScene(scene);
+			primaryStage.show();		
+			
+		}
+		catch(Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
 	private void createTable()
 	{
+		@SuppressWarnings("rawtypes")
 		TableColumn examinationColumn = new TableColumn("Badanie");
 		
 		examinationColumn.setCellValueFactory(new Callback<CellDataFeatures<UserData, CheckBox>, ObservableValue<CheckBox>>() 
@@ -70,7 +172,7 @@ public class UserApplication extends Application {
 		
 				
 		TableColumn<UserData, String> fullNameColumn = new TableColumn<>("Imiê i nazwisko");
-		fullNameColumn.setMinWidth(300);
+		fullNameColumn.setMinWidth(200);
 		fullNameColumn.setCellValueFactory(new PropertyValueFactory<UserData, String>("fullName"));
 		
 		TableColumn<UserData, String> sexColumn = new TableColumn<>("P³eæ");
@@ -78,7 +180,7 @@ public class UserApplication extends Application {
 		sexColumn.setCellValueFactory(new PropertyValueFactory<UserData, String>("sex"));
 		
 		TableColumn<UserData, String> idColumn = new TableColumn<>("PESEL");
-		idColumn.setMinWidth(300);
+		idColumn.setMinWidth(200);
 		idColumn.setCellValueFactory(new PropertyValueFactory<UserData, String>("id"));
 		
 		TableColumn<UserData, String> insuranceColumn = new TableColumn<>("Ubezpieczenie");
@@ -99,83 +201,6 @@ public class UserApplication extends Application {
               
         });
     }
-	
-
-	
-	@Override
-	/*
-	 * (non-Javadoc)
-	 * @see javafx.application.Application#start(javafx.stage.Stage)
-	 */
-	public void start(Stage primaryStage)
-	{
-		try 
-		{
-			createTable();
-			BorderPane mainLayout = new BorderPane();
-			VBox layout = new VBox(10); // layout z ekspozycja w srodku
-			layout.setPadding(new Insets(10, 10, 10 ,10));
-			button = new Button("Start"); // moze byc tez setText()
-			layout.getChildren().add(button);
-			
-			text = new TextField();
-			layout.getChildren().add(text);
-			
-			label = new Label("");
-			label.setWrapText(true);
-			layout.getChildren().add(label);
-			
-			mainLayout.setCenter(layout);
-
-			MenuItem menuItem = new MenuItem("Wyjœcie");
-			menuItem.setOnAction(e->
-			{
-				
-				primaryStage.close();
-				
-			});
-			
-			menu.getItems().add(menuItem);
-			menuBar.getMenus().add(menu);
-			
-			mainLayout.setTop(menuBar);
-
-			button.setOnAction(e->
-			{
-				
-				label.setText(controller.getData(text.getText()));
-				
-			});
-	
-		
-			
-			/*table.setItems(getTable());
-			table.getColumns().addAll(fullNameColumn, sexColumn, idColumn, insuranceColumn, examinationColumn);*/
-			
-			VBox vBoxLayout = new VBox(); 
-			vBoxLayout.getChildren().add(table);
-			mainLayout.setRight(vBoxLayout);
-
-			
-		
-			Scene scene = new Scene(mainLayout, 1200, 450);
-			
-			primaryStage.setTitle("Rejestracja wyników badañ");
-			primaryStage.setScene(scene);
-			primaryStage.show();		
-			
-		} catch(Exception e) 
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	/*public ObservableList<Table> getTable()
-	{
-		ObservableList<Table> table = FXCollections.observableArrayList();
-		table.add(new Table("Tomasz Wa³êsa", 'M', "68051307545", "NFZ", true));
-		return table;
-	}*/
 	
 	public static void main(String[] args)
 	{
